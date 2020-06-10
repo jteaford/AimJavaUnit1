b3aef87355ef5fdc37618dcec64774d5a1af2767
Jessica Teaford <jessicateaford@Jessicas-MacBook-Pro-2.local>
2020-06-09 21:16:06 -0500

    Initial Commit

---
+++ src/Assignment1_1.java
@@ -0,0 +1,8 @@
+public class Assignment1_1 {
+    public static void main(String[] args) {
+        System.out.println("Jessica Teaford");
+        System.out.println("Java Web");
+        System.out.println("Omaha, NE");
+        System.out.println("Coneflower Ice Cream");
+    }    
+}
